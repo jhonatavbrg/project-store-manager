35decfc3302096c0712c201f891fa592cbddc037
jhonatavbrg <jhonatavbrg@gmail.com>
2021-10-19 15:31:21 -0300

    Initial commit

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-// não remova esse endpoint, e para o avaliador funcionar
+const express = require('express');
+const bodyParser = require('body-parser').json();
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser);
+
 app.get('/', (_request, response) => {
   response.send();
+});
+
+app.listen(PORT, () => {
+  console.log(`running at the door ${PORT}`);
 });
